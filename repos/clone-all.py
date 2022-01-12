cd7facf43b2fb11760b047e4e8d8319513df7b67
Michael Eggers <mseggers@protonmail.ch>
2022-01-12 11:29:34 +0100

    add repos as submodules

--- repos/clone-all.py
+++ repos/clone-all.py
@@ -4,9 +4,9 @@ NUM_REPOS = 25
 
 repos = []
 for i in range(NUM_REPOS):
-    repos.append("git@github.com:CT-WS2021-Art/generative-kunst-team-" + str(i) + ".git")
+    repos.append("https://github.com/CT-WS2021-Art/generative-kunst-team-" + str(i))
 
 for repo in repos:
-    subprocess.run(["git", "clone", repo])
+    subprocess.run(["git", "submodule", "add", repo])
 
-    subprocess.run(["git", "clone", "git@github.com:CT-WS2021-Art/Team-17-richtig-krass.git"])
+subprocess.run(["git", "submodule", "add", "https://github.com/CT-WS2021-Art/Team-17-richtig-krass"])
